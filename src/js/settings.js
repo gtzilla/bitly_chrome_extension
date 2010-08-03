8fd17e21d5922d727ad4a2d0bd4f56212ec37c1a
gregory80 <gregory.tomlinson@gmail.com>
2010-08-03 14:25:07 -0400

    notes and cleanup, prep for merge

--- src/js/settings.js
+++ src/js/settings.js
@@ -3,5 +3,10 @@
     'chrome_key' : 'R_042ce7bde03d756e0448b28b1f2c4aa3',
     'host' : 'api.bit.ly',
     'user_table' : 'users_t',
-    db_version : 1
+    'db_version' : 1
+    
+    /*
+        note this is currently not is use b/c of a bug in chrome 
+        that prevents xml load of this file
+    */
 }
