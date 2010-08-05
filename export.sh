d8e6dfaf0350cf0f335be856bd3c6c07c1b27bd0
Jehiah Czebotar <jehiah@gmail.com>
2010-08-05 14:19:15 -0400

    remove website directory; let extension be hosted on chrome.google.com

--- export.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-mkdir -p /bitly/local/bitly_chrome_extension
-echo "deploying bitly_chrome_extension"
-/bitly/local/bin/git checkout-index -a -f --prefix=/bitly/local/bitly_chrome_extension/
