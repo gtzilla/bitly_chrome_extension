9ded5dd0e58f05a49f5e7482cda25e0219583d7b
Jehiah Czebotar <jehiah@gmail.com>
2010-08-05 00:03:27 -0400

    stub out website (needs styling)

--- deploy_extension.sh
+++ deploy_extension.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-HOST_PORT_FILES="js/bitly_settings.js js/plugins/jquery.connector.js js/workers/screen_resizer.js"
 HOST_FILES="manifest.json"
 CWD=`pwd`
 #KEY="/PATH/to/chrome_extension.pem"
@@ -25,11 +24,11 @@ VERSION=`cat src/manifest.json | grep '"version"' | awk -F '"' '{print $4}'`
 echo "new extension version is $VERSION"
 
 # update in the updates.xml file
-perl -pi -e "s/version=\"(\d\.)+\d+\"\s\/>/version=\"$VERSION\" \/>/g" "website/chrome_extension_updates.xml"
+perl -pi -e "s/version=\"(\d\.)+\d+\"\s\/>/version=\"$VERSION\" \/>/g" "website/static/chrome_extension_updates.xml"
+echo "__version__ = \"$VERSION\"" > website/current_version.py;
 
-echo "compiling chrome_extension.crx";
+echo "compiling bitly_chrome_extension.crx";
 "$CHROME" --pack-extension=$CWD/src --pack-extension-key=$KEY --no-message-box
-cp "$CWD/src.crx" website/chrome_extension.crx
-rm "$CWD/src.crx"
-echo "built chrome_extension.crx";
+mv "$CWD/src.crx" "$CWD/bitly_chrome_extension-$VERSION.crx"
+echo "built bitly_chrome_extension-$VERSION.crx";
 
