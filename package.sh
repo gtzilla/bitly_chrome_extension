1bf001c671fc24f418e34aa141eb5ad88e3a5d4c
gregory80 <gregory.tomlinson@gmail.com>
2011-02-17 13:32:22 -0500

    ext rev

--- package.sh
+++ package.sh
@@ -30,8 +30,37 @@ perl -pe 's/(\s+.version.: .\d+\.\d+\.\d+\.)(\d+)/$1.($2+1)/eg' -i "src/manifest
 VERSION=`cat src/manifest.json | grep '"version"' | awk -F '"' '{print $4}'`
 echo "new extension version is $VERSION"
 
+## compress JS and use that for zip..
+## todo
+## cp -r src src_alter
+## for file in src/js/*.js; do echo $file; done;
+## for file in src/js/content_plugins/*.js; do echo $file; done;
+## for file in src/js/libs/*.js; do echo $file; done;
+## do
+COMPRESS_JAR="../projects/website/tools/closure_compiler.jar"
+# cp -r src src_alter
+# if [ ! -f "$COMPRESS_JAR" ]; then
+#     echo "no closure compule, skipping"
+# else
+#     echo "build compressed files"
+# 
+#     for file in src_alter/js/*.js; 
+#         do java -jar "$COMPRESS_JAR" --js=$file --js_output_file=$file
+#     done;
+#     for file in src_alter/js/content_plugins/*.js; 
+#         do java -jar "$COMPRESS_JAR" --js=$file --js_output_file=$file
+#     done;       
+#     for file in src_alter/js/libs/*.js; 
+#         do java -jar "$COMPRESS_JAR" --js=$file --js_output_file=$file
+#     done;     
+#     
+# fi
+##  java -jar ../projects/website/tools/closure_compiler.jar --js=$file --js_output_file=$file
+## done;
+
 ## remove the sample file and any Mac specific files
 zip -q -r "bitly_ext-$VERSION.zip" "$CWD/src" --exclude \*.DS_Store \*bitly_oauth_credentials.js.sample
+# rm -rf src_alter
 
 echo "upload: bitly_ext-$VERSION.zip"
 exit 0;
