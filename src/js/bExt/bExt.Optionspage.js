c6fd799a709b38ae18d4fe824dc276aba64764ca
gregory80 <gregory.tomlinson@gmail.com>
2011-04-28 15:54:27 -0400

    all about the options

--- src/js/bExt/bExt.Optionspage.js
+++ src/js/bExt/bExt.Optionspage.js
@@ -9,23 +9,96 @@
 
 (function( window, undefined) {
     
+/*
+    DOM Representation of the Settings / Options page (builtin chrome options page)
+    
+        Controls User Meta Data Settings and Configuration
+        
+        As or ver Chrome Ext ver 1, the sign in screen shared the same page as this page.
+*/
 window.bExt.Optionspage = function() {
     
-    console.log("built!")
+    console.log("built!");
     return this;
 }
+
 window.bExt.Optionspage.prototype={
     
+    __lst : [],
     
-    basic : function() {
+    trends : function() {
         
+        var frag, opts_page_meta = new bExt.OptionMeta({
+            title : "Trend Notifications",
+            label : "Enable Notifications",
+            desc : "Automatically notify me when my link starts to become popular, or trend. Notifications will be shown when a link reaches the threshold specified below during the past hour."
+        });        
         
-        var opts_page_meta = bExt.OptionMeta({
+        frag=single_check_frag( opts_page_meta );
+        var trend_frag_details = [{
+            css : "smallInputContainer notificationInnerContainer",
+            content : [{
+                type : "form",
+                id : "notifications_form",
+                attrs : {
+                    action : "#",
+                    method : "get",
+                    "accept-charset" : "utf-8"
+                },
+                content : [{
+                    type : "label",
+                    content : "Default Click Threshold"
+                }, {
+                    type : "input",
+                    attrs : {
+                        type : "text",
+                        value : 20
+                    }
+                }, {
+                    type : "input",
+                    attrs : {
+                        value : "Update",
+                        type : "submit"
+                    }
+                }]
+            },{
+                type : "p",
+                content : "Note: Threshold can be any integer from 5-5,000"
+            }]
+        }]
+        frag.content=frag.content.concat(trend_frag_details);
+        fastFrag.create( frag );
+    },
+    
+    auto_copy : function() {
+        
+        var opts_page_meta = new bExt.OptionMeta({
+            title : "Auto Copy Short Urls",
+            desc : "Automatically copy short urls to my clipboard when popup opens"
+        });        
+        
+        return build( opts_page_meta );       
+    },
+    
+    twitter : function() {
+        var opts_page_meta = new bExt.OptionMeta({
             title : "Twitter Enhance",
             label : "Enhance Twitter",
             desc : "Display a shorten button on twitter.com when I enter a long URL"
-        });
+        });  
+        
+        
+        return build( opts_page_meta );
+    },
+    
+    basic : function() {
+        console.log("basic: window.bExt.Optionspage");
     }
+}
+
+function build( opts_meta ) {
+    var frag = single_check_frag( opts_meta.out() );
+    return fastFrag.create( frag );    
 }
 
 function single_check_frag( meta ) {
@@ -39,6 +112,7 @@ function single_check_frag( meta ) {
     */
     
     return {
+        css : "options_container",
         content : [{
             type : "h3",
             content : meta.title
@@ -79,14 +153,28 @@ function single_check_frag( meta ) {
 
 window.bExt.OptionMeta = function( meta_obj  ) {
     this.__m=$.extend( true, this.__m, meta_obj );
-    if(!this.__m.id) {
-        this.__m.id=(this.__m.title + "_" + this.__m.label).replace(/[^a-z0-9]/gi, "_").toLowerCase();
-    }
-    
+
+    this.set_id();
+    this.set_label();
+
     return this;
 }
 
 window.bExt.OptionMeta.prototype = {
+    
+    set_id : function() {
+        if(!this.__m.id) {
+            this.__m.id=(this.__m.title + "_" + this.__m.label).replace(/[^a-z0-9]/gi, "_").toLowerCase();
+        }        
+    },
+    set_label : function() {
+        if(!this.__m.label) {
+            this.__m.label=this.__m.title;
+        }
+    },
+    out : function() {
+        return this.__m;
+    },
     __m : {
         id : null,
         title : null,
