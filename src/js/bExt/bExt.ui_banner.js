a95aa95ffb3cba74a26e04d6c155a2b26c6b4f59
gregory80 <gregory.tomlinson@gmail.com>
2011-05-03 23:54:51 -0400

    draw banner in JS

---
+++ src/js/bExt/bExt.ui_banner.js
@@ -0,0 +1,39 @@
+// 
+//  bExt.ui_banner.js
+//  bitly_chrome_extension
+//  
+//  Created by gregory tomlinson on 2011-05-03.
+//  Copyright 2011 the public domain. All rights reserved.
+// 
+
+(function(window, undefined){
+
+
+var settings = {
+    box : "#top",
+    delay : 20
+}
+
+window.bExt.ui_banner = {
+    
+    init : function( opts ) {
+        settings=jQuery.extend({}, settings, opts);
+        $(settings.box).prepend( fastFrag.create( frag_struct() ) );        
+        setTimeout(function(){
+            $(document.body).addClass("page_loaded");
+        },  settings.delay || 20 ); // must be at least 1
+    }
+    
+}
+
+function frag_struct() {
+    return {
+        id : "blueBanner",
+        content : {
+            type : "ul",
+            css : "ext_nav_list"
+        }
+    }
+}
+    
+})(window);
