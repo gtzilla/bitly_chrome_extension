f5aaf5376d0f818cbaafa7444f8b9ff679ecfc82
gregory80 <gregory.tomlinson@gmail.com>
2011-05-06 12:09:36 -0400

    notes on new framerate api to try

--- src/js/bExt/bExt.metrics.js
+++ src/js/bExt/bExt.metrics.js
@@ -7,22 +7,90 @@
 // 
 
 (function(window, undefined){
-    
+
+var settings = {
+    box : "#middle"
+}
+// framing.. for animation... hmmm
 window.bExt.metrics = {
-    
-    init : function() {
-        
+    /*
+        Info
+            
+            This API is planning to use the new window.webkitRequestAnimationFrame API
+                
+                This became available in chrome 10
+                    https://developer.mozilla.org/en/DOM/window.mozRequestAnimationFrame#AutoCompatibilityTable
+            
+            Mozilla docs
+                
+                https://developer.mozilla.org/en/DOM/window.mozRequestAnimationFrame
+    */
+    init : function( opts ) {
+        settings=jQuery.extend(true, {}, settings, opts );
         var r_meta = bExt.info.get("realtime") || {},
             popup_history=bExt.info.get("popup_history") || [];
             
         console.log("realtime", r_meta);
         console.log("popup", popup_history);
         
+
+    },
+    
+    assemble : function() {
+        $(settings.box).append(fastFrag.create(search_frag() ) );        
+    }
+    
+    
+}
+
+function search_frag() {
+    
+    return {
+        content : [{
+            type : "form",
+            content : [{
+                type : "input",
+                id : "search_links",
+                attr : {
+                    type : "text",
+                    name : "search_links",
+                    value : "Search for links"
+                }
+            }, {
+                type : "input",
+                attr : {
+                    type : "submit",
+                    value : "Add to Chart"
+                }
+            }]
+        }]
+    }
+    
+}
+
+
+window.bExt.metrics.Meta = function( opts ) {
+    this.__m=jQuery.extend(true, {}, this.__m, opts);
+}
+
+window.bExt.metrics.Meta.prototype =  function() {
+    
+    graph : function( ctx ) {
+        // ctx == context for canvas element
+    },
+    
+    points : function() {
+        // track the elemt points and their divs
         
     },
     
+    // the event method and context to use when handling events
+    "event_method" : null
     
     
+    __m = {
+        
+    }
     
 }
     
